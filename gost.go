d0aae00eca74467ed0a988491062720fea8a7f2c
elCuervo <yo@brunoaguirre.com>
2014-02-16 22:14:01 -0200

    Go version of Ost

---
+++ gost.go
@@ -0,0 +1,116 @@
+package gost
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"sync"
+)
+
+type queue struct {
+	Key    string
+	Backup string
+	Stop   bool
+
+	conn redis.Conn
+}
+
+func (q *queue) push(id string) {
+	q.conn.Do("LPUSH", q.Key, id)
+}
+
+func (q *queue) items() []string {
+	items, _ := redis.Strings(q.conn.Do("LRANGE", q.Key, 0, -1))
+	return items
+}
+
+type caller func(string) bool
+
+func (q *queue) each(fn caller) {
+	for {
+		if q.Stop == true {
+			break
+		}
+
+		item, err := redis.String(q.conn.Do("BRPOPLPUSH", q.Key, q.Backup, 2))
+
+		if err != nil {
+			continue
+		}
+
+		success := fn(item)
+
+		if success {
+			q.conn.Do("LPOP", q.Backup)
+		}
+
+	}
+}
+
+type Gost struct {
+	Prefix string
+	Redis  redis.Conn
+	mutex  sync.Mutex
+	queues map[string]*queue
+}
+
+func Create(url string) *Gost {
+	g := new(Gost)
+	g.queues = make(map[string]*queue)
+	g.Prefix = "gost:queues"
+
+	conn, err := redis.Dial("tcp", url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	g.Redis = conn
+
+	return g
+}
+
+func (g *Gost) createQueue(name string) *queue {
+	q := new(queue)
+	q.Key = g.Prefix + ":" + name
+	q.Backup = q.Key + ":backup"
+	q.conn = g.Redis
+
+	return q
+}
+
+func (g *Gost) Push(queue_name string, id string) {
+	queue := g.get_queue(queue_name)
+	queue.push(id)
+}
+
+func (g *Gost) get_queue(queue_name string) *queue {
+	queue_id := g.Prefix + ":" + queue_name
+
+	g.mutex.Lock()
+	queue := g.queues[queue_id]
+	g.mutex.Unlock()
+
+	if queue == nil {
+		queue = g.createQueue(queue_name)
+		g.mutex.Lock()
+		g.queues[queue_id] = queue
+		g.mutex.Unlock()
+	}
+
+	return queue
+}
+
+func (g *Gost) Each(queue_name string, fn caller) {
+	queue := g.get_queue(queue_name)
+	queue.each(fn)
+}
+
+func (g *Gost) Items(queue_name string) []string {
+	queue := g.get_queue(queue_name)
+	return queue.items()
+}
+
+func (g *Gost) Stop() {
+	for _, queue := range g.queues {
+		queue.Stop = true
+	}
+}
---
+++ gost_test.go
@@ -0,0 +1,42 @@
+package gost
+
+import (
+	"git.tideland.biz/gots/asserts"
+	"github.com/garyburd/redigo/redis"
+	"testing"
+	"time"
+)
+
+func init() {
+        conn, _ := redis.Dial("tcp", ":6379")
+        conn.Do("FLUSHDB")
+}
+
+func TestAccessToQueue(t *testing.T) {
+	assert := asserts.NewTestingAssertion(t, true)
+	g := Create(":6379")
+
+	g.Push("my_queue", "1")
+	g.Push("my_queue", "2")
+
+	items := []string{"2", "1"}
+
+	assert.Equal(g.Items("my_queue"), items)
+}
+
+func TestReadingQueue(t *testing.T) {
+	g := Create(":6379")
+	g.Prefix = "test:queues"
+
+	g.Push("my_queue", "1")
+
+	go func() {
+		time.Sleep(time.Millisecond * 500)
+		g.Stop()
+	}()
+
+	g.Each("my_queue", func(id string) bool {
+		return true
+	})
+
+}
